8d1b3b58161770680a0744d647395243758c1e99
Sergey Vlasov <sergey_vlasov@yahoo.com>
2017-01-21 15:23:49 -0500

    heroku web fix

--- slack_bot.js
+++ slack_bot.js
@@ -80,6 +80,15 @@ var bot = controller.spawn({
     token: process.env.SLACK_TOKEN
 }).startRTM();
 
+
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
+
 controller.hears(['hello', 'hi'], 'direct_message,direct_mention,mention', function(bot, message) {
 
     bot.api.reactions.add({
